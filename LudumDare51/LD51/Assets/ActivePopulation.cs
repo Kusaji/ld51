cfcd6ee823988468d8d8207dac10a75eedda6e57
Albritton <albritton2020@gmail.com>
2022-10-01 03:06:58 -0700

    farmstuff
    ahhh I hate pushing

---
+++ LudumDare51/LD51/Assets/ActivePopulation.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ActivePopulation : MonoBehaviour
+{
+    public TextMeshProUGUI populationTMP;
+    public Canvas theCanvas;
+    // Update is called once per frame
+    void Update()
+    {
+        //theCanvas.transform.LookAt(CameraController.instance.theCamera.transform);
+        //Vector3 lookie = theCanvas.transform.eulerAngles;
+        //theCanvas.transform.rotation = Quaternion.Euler(lookie.x + 180f, lookie.y, lookie.z + 180f);
+        
+        
+        theCanvas.transform.rotation = CameraController.instance.theCamera.transform.rotation;
+    }
+}
