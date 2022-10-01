b54da6c5f7bf3ca4e3fd3155861919aacdb3ae1e
Zack <kusajiproduction@gmail.com>
2022-09-30 21:27:19 -0400

    Start to scripting

---
+++ LudumDare51/LD51/Assets/Scripts/Player/PlayerResources.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerResources : MonoBehaviour
+{
+    public static PlayerResources instance;
+    
+    public int population;
+    public int builders;
+    public int repairmen;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+   
+
+    public void TakeDamage(float damage)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
