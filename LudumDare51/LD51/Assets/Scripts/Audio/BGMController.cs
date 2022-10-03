c0c2663387c9fc4dc82de2c4306c82f4f6f2420e
Zack <kusajiproduction@gmail.com>
2022-10-03 03:39:13 -0400

    Pause menu

--- LudumDare51/LD51/Assets/Scripts/Audio/BGMController.cs
+++ LudumDare51/LD51/Assets/Scripts/Audio/BGMController.cs
@@ -10,7 +10,7 @@ public class BGMController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartPlayingRandomSong();
+        StartCoroutine(CheckIfSongEndedRoutine());
     }
 
 
